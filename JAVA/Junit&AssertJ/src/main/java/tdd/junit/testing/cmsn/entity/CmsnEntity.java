12268674e52849b0b80e71cdd6f808248b036698
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2024-02-29 11:29:00 +0900

    feat: add Junit&AssertJ files

---
+++ JAVA/Junit&AssertJ/src/main/java/tdd/junit/testing/cmsn/entity/CmsnEntity.java
@@ -0,0 +1,11 @@
+package tdd.junit.testing.cmsn.entity;
+
+import lombok.Data;
+
+@Data
+public class CmsnEntity {
+    private Integer etrdMallId;
+    private Integer regionCd;
+    private String mallNm;
+    private Integer rvnAmnt;
+}
